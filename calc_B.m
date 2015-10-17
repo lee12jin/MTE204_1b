8c0ef74653ab5dcf6f198f63a44e3ae2e581d5f2
genie kim <geniekim@gmail.com>
2015-10-17 17:20:00 -0400

    combined A B D matrices funcs to one for explicit dynamics

--- calc_B.m
+++
@@ -1,6 +0,0 @@
-function [ B ] = calc_B( k, M, t_delta )
-    % calculates [B] matrix for Explicit Dynamics
-    B = k - 2*M/(t_delta*t_delta);
-end
-
-
--- calc_D.m
+++
@@ -1,5 +0,0 @@
-function [ D ] = calc_D( M, C, t_delta )
-    % calculates [D] matrix for Explicit Dynamics
-    D = (M/(t_delta*t_delta)) - (C/(2*t_delta));
-end
-
