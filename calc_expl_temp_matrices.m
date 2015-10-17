925aaf59ac53533812b50e917a8422592a891380
lodann <dalo@uwaterloo.ca>
2015-10-17 19:59:30 -0400

    change k to K

--- calc_expl_temp_matrices.m
+++ calc_expl_temp_matrices.m
@@ -1,7 +1,7 @@
-function [ A, B, D ] = calc_expl_temp_matrices( M, C, k, t_delta )
+function [ A, B, D ] = calc_expl_temp_matrices( M, C, K, t_delta )
     % calculates [A] matrix for Explicit Dynamics
     A = (M/(t_delta*t_delta)) + (C/(2*t_delta));
-    B = k - 2*M/(t_delta*t_delta);
+    B = K - 2*M/(t_delta*t_delta);
     D = (M/(t_delta*t_delta)) - (C/(2*t_delta));
     
 end
