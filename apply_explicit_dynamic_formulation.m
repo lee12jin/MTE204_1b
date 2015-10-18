d7d83b78744068335b69f81a183e10fb62041c82
lodann <dalo@uwaterloo.ca>
2015-10-17 22:51:50 -0400

    working explicit

--- apply_explicit_dynamic_formulation.m
+++ apply_explicit_dynamic_formulation.m
@@ -1,35 +1,27 @@
-function [ u, v, a, f ] = apply_explicit_dynamic_formulation( A_t, B_t, D_t, u0, u_next, v0, a0, t, time_step, n )
+function [ u, v, a, f ] = apply_explicit_dynamic_formulation( A_t, B_t, D_t, u0, u_unknown, v0, a0, f_unknown, t, time_step, n )
 %APPLY_EXPLICIT_DYNAMIC_FORMULATION Summary of this function goes here
 %   Detailed explanation goes here
     syms u_2 f_1;
     
-    u = zeros(n, t / time_step);
-    v = zeros(n, t / time_step);
-    a = zeros(n, t / time_step);
-    f = zeros(n, t / time_step);
+    len = t / time_step + 1;
     
+    u = zeros(n, len);
+    v = zeros(n, len);
+    a = zeros(n, len);
+    f = zeros(n, len);
+    
+    % Time zero conditions
     u(:, 1) = u0;
     v(:, 1) = v0;
     a(:, 1) = a0;
-    f(:, 1) = zeros(n, 1);
+    f(:, 1) = [0;
+               10;];
     
-    for i = 1:(t / time_step)
-        if (i > 1)
-            u_prev = u(:, i - 1);
-        else
-            u_prev = u(:, 1);
-        end
-        
-        f_cur = [f_1;
-                 10;];
-        
-        eqn = (A_t * u_next) == (f_cur - B_t * u(:, i) - D_t * u_prev);
+    for i = 1:len
+        eqn = (A_t * u_unknown) == (f_unknown - B_t * u(:, i) - D_t * u(:, max(1, i - 1)));
         
         values = solve(eqn, u_2, f_1);
-        
-        u2 = double(values.u_2);
-        f1 = double(values.f_1);
-        
+                
         % Next displacement
         u(:, i + 1) = [0;
                        values.u_2;];
@@ -39,11 +31,16 @@ function [ u, v, a, f ] = apply_explicit_dynamic_formulation( A_t, B_t, D_t, u0,
                    10;];
         
         % Calculate velocity
-        v(:, i) = ( u(:, i + 1) - u_prev ) / (2 * time_step);
+        v(:, i) = ( u(:, i + 1) - u(:, max(1, i - 1)) ) / (2 * time_step);
         % Calculate acceleration
-        a(:, i) = ( u(:, i + 1) - 2 * u(:, i) + u_prev ) / (time_step ^ 2);
+        a(:, i) = ( u(:, i + 1) - 2 * u(:, i) + u(:, max(1, i - 1)) ) / (time_step ^ 2);
         
     end
+    
+    u = u(:, 2:len);
+    v = v(:, 2:len);
+    a = a(:, 2:len);
+    f = f(:, 2:len);
 
 end
 
--- question1.m
+++ question1.m
@@ -32,14 +32,14 @@ k = [10];
 % Initial conditions for Node 2
 u0 = [0;
       0;]; % displacement [m]
-u1 = [0;
-      u_2;]; % displacement after first timestep
+u_unknown = [0;
+          u_2;]; % displacement after first timestep
 v0 = [0;
       0;]; % velocity [m/s]
 a0 = [0;
       0;];% acceleration [m/s^2]
-f1 = [f_1;
-      10;]; % force [N]
+f_unknown = [f_1;
+          10;]; % force [N]
 
 % Size of the global matrices
 size = dof * n;
@@ -67,9 +67,9 @@ t3 = linspace(0, t, t/time_step3);
 [ A_t2, B_t2, D_t2 ] = calc_expl_temp_matrices( M, C, K, time_step2 );
 [ A_t3, B_t3, D_t3 ] = calc_expl_temp_matrices( M, C, K, time_step3 );
 
-[u_solved1, v_solved1, a_solved1, f_solved1] = apply_explicit_dynamic_formulation(A_t1, B_t1, D_t1, u0, u1, v0, a0, t, time_step1, n);
-[u_solved2, v_solved2, a_solved2, f_solved2] = apply_explicit_dynamic_formulation(A_t2, B_t2, D_t2, u0, u1, v0, a0, t, time_step2, n);
-[u_solved3, v_solved3, a_solved3, f_solved3] = apply_explicit_dynamic_formulation(A_t3, B_t3, D_t3, u0, u1, v0, a0, t, time_step3, n);
+[u_solved1, v_solved1, a_solved1, f_solved1] = apply_explicit_dynamic_formulation(A_t1, B_t1, D_t1, u0, u_unknown, v0, a0, f_unknown, t, time_step1, n);
+[u_solved2, v_solved2, a_solved2, f_solved2] = apply_explicit_dynamic_formulation(A_t2, B_t2, D_t2, u0, u_unknown, v0, a0, f_unknown, t, time_step2, n);
+[u_solved3, v_solved3, a_solved3, f_solved3] = apply_explicit_dynamic_formulation(A_t3, B_t3, D_t3, u0, u_unknown, v0, a0, f_unknown, t, time_step3, n);
 
 % Part b
 % gamma = 3 / 2;
