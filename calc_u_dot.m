e391f7ac9d32bc9ad52ab111db78b736ffe27430
lee12jin <lee12jin@gmail.com>
2015-10-18 22:16:36 -0400

    Delete useless files.

--- calc_u_dot.m
+++
@@ -1,9 +0,0 @@
-function [ u_ ] = calc_u_dot( u1, u_1, t_delta )
-    % calculates [u.]i for Explicit Dynamics
-    % u1 = [u]i+1
-    % u_1 = [u]i-1
-    % t_delta = time step
-   
-    u_ = (u1 - u_1) / (2*t_delta);
-end
-
--- calc_u_dotdot.m
+++
@@ -1,9 +0,0 @@
-function [ u__ ] = calc_u_dotdot( u1, u, u_1, t_delta )
-    % calculates [u..]i for Explicit Dynamics
-    % u1 = [u]i+1
-    % u = [u]i
-    % u_1 = [u]i-1
-    % t_delta = time step
-    u__ = (u1 - 2*u + u_1) / (t_delta*t_delta);
-end
-
